93825b76adb128a2a614e5e056b836d7b77c7964
André Pinto <andredsp@alumni.usp.br>
2021-06-29 22:31:39 -0300

    Show only current attendances

--- daimoku/resources/views/room.blade.php
+++ daimoku/resources/views/room.blade.php
@@ -24,9 +24,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <h2> Participantes </h2>
+                    <h2> Participantes Atuais </h2>
                     <ul>
-                        @foreach ($room->attendances as $attendance)
+                        @foreach ($room->attendances()->whereNull('left_at')->get() as $attendance)
                         <li>{{ $attendance->user->name }}</li>
                         @endforeach                 
                     </ul> 
